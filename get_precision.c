f24b8a6698422fdea8966130f83428c65bf32e6c
oussama zaima <oussama.zaima2002@gmail.com>
2023-10-17 07:16:04 -0700

    fixed

--- get_precision.c
+++ get_precision.c
@@ -6,14 +6,14 @@
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
- * Return: Precision.
+ * Return: precision.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
 	int precision = -1;
 
-	if (format[curr_i] != '.')
+	if (format[curr_i] != '*')
 		return (precision);
 
 	precision = 0;
